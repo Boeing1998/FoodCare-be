ff73bde42efa537521d1a4eec48b2e59c4cbd89b
Trung <quoctrung.tran210@gmail.com>
2021-04-07 03:12:19 +0700

    add PORT for heroku

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
 server.listen(port, () => {
     console.log(`http://localhost:${port}`)
+});
+
+server.listen(PORT, () => {
+    console.log(`server is up and listening on:  http://localhost:${PORT}`)
 });
