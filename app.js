6f6d989f87f0523e3fc160ead66f97d7a953be75
Trung <quoctrung.tran210@gmail.com>
2021-04-07 20:06:36 +0700

    test bo dotenv

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-require('dotenv').config();
+// require('dotenv').config();
 
 const userRoutes = require("./src/routes/user.routes");
 const { json } = require("express");
