0a029d2f93d16b61b97d1186f66bcb78c9c8523f
Trung <quoctrung.tran210@gmail.com>
2021-05-06 00:41:09 +0700

    Edit user and login-logout add-del token in db

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,14 +4,22 @@ const router = express.Router();
 const UserController = require('../controllers/user.controllers');
 const checkAuth = require('../helper/check-auth');
 
-router.post("/signup", UserController.user_signup);
+const RegisterValidation = require('../validations/register.validation');
+
+router.post("/signup", RegisterValidation, UserController.user_signup);
 
 router.post("/login", UserController.user_login);
 
-// router.delete("/logout", checkAuth, UserController.logout )
+router.delete("/logout", checkAuth, UserController.logout )
+
+// router.get("/:userId", checkAuth, UserController)
+
+router.get("/profile", checkAuth, UserController.showDetail );
+
+router.put("/edit", checkAuth, UserController.editUser );
 
 // router.delete("/:userId", checkAuth, UserController.user_delete);
 
-// router.put("e");
+
 
 module.exports = router;
